6a76e005625a05f046230b8498ed615c9c4e3241
Matin_Ghanbari <ghanbarimatin6@gmail.com>
2024-08-30 02:15:08 +0330

    [LexiService]: Add Dockerfile

--- src/LexiService/ApiServer/Program.cs
+++ src/LexiService/ApiServer/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+Console.WriteLine(builder.Configuration.GetChildren());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

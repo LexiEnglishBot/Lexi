08739709b4d0766b44f605baa6ed8018bc1d1b42
Matin_Ghanbari <ghanbarimatin6@gmail.com>
2024-08-30 02:46:28 +0330

    [LexiService]: Add aggregate root ctor

--- src/LexiService/Domain/AggregateRoot.cs
+++ src/LexiService/Domain/AggregateRoot.cs
@@ -3,8 +3,16 @@
 public abstract class AggregateRoot<T>
 {
     public T Id { get; set; }
+
+    protected AggregateRoot(T id)
+    {
+        Id = id;
+    }
 }
 
 public class AggregateRoot : AggregateRoot<Guid>
 {
+    public AggregateRoot(Guid id) : base(id)
+    {
+    }
 }

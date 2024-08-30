5ff20ee514d45200536a33408d6053d0ccb0089f
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-08-30 20:19:35 +0330

    [LexiService]: Add UpdateHelpers

---
+++ src/LexiService/ApplicationService/Services/ServiceHelpers/UpdateHelpers.cs
@@ -0,0 +1,25 @@
+﻿
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ApplicationService.Services.ServiceHelpers;
+
+public static class UpdateHelpers
+{
+    public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        // Only process Message updates
+        if (update.Message is not { } message)
+        {
+            return;
+        }
+
+        // Only process text messages
+        if (message.Text is not { } messageText)
+        {
+            return;
+        }
+
+        Console.WriteLine($"Received a '{messageText}' message in chat {message.Chat.Id}.");
+    }
+}

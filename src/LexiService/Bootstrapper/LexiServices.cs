119dfe6672ce146d49320a5e18ba4a1a85ed7826
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-08-30 20:07:29 +0330

    [LexiService]: Add RegisterTelegramServices

---
+++ src/LexiService/Bootstrapper/LexiServices.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationService.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bootstrapper;
+
+public static class LexiServices
+{
+    public static IServiceCollection RegisterTelegramServices(this IServiceCollection services)
+    {
+        return services.AddHostedService<TelegramService>();
+    }
+}

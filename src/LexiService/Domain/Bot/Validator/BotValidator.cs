ac3afaaa15c9af055e29c386d1d732822df8c2be
Matin_Ghanbari <ghanbarimatin6@gmail.com>
2024-08-31 04:18:28 +0330

    [LexiService]: Add bot validator

---
+++ src/LexiService/Domain/Bot/Validator/BotValidator.cs
@@ -0,0 +1,20 @@
+﻿using Core.Resources.Exceptions;
+using Domain.Bot.Aggregate;
+using Domain.Bot.ValueObjects;
+using FluentValidation;
+
+namespace Domain.Bot.Validator;
+
+public class BotValidator : AbstractValidator<BotAggregate>
+{
+    public BotValidator()
+    {
+        RuleFor(x => x.Id).NotNull().NotEqual(Guid.Empty)
+            .WithMessage(ExceptionMessages.INVALID_BOT_ID);
+
+        RuleFor(x => x.Apikey).Must(ValidateApikey)
+            .WithMessage(ExceptionMessages.INVALID_API_KEY);
+    }
+
+    private static bool ValidateApikey(ApiKey apiKey) => apiKey.IsValid().IsSuccess;
+}

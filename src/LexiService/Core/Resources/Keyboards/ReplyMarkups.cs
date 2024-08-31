76ea9a4162e4ca9987feccd4b02f6e7f61d77c51
Matin_Ghanbari <ghanbarimatin6@gmail.com>
2024-08-31 04:17:44 +0330

    [LexiService]: Add keyboard resources

---
+++ src/LexiService/Core/Resources/Keyboards/ReplyMarkups.cs
@@ -0,0 +1,24 @@
+﻿using System.Resources;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Core.Resources.Keyboards;
+
+public class ReplyMarkups
+{
+    public Dictionary<string, IReplyMarkup> ReplyMarkupsDictionary;
+
+    public ReplyMarkups(ResourceManager keyboardResourceManager, ResourceManager keyboardDataResourceManager)
+    {
+        ReplyMarkupsDictionary = new Dictionary<string, IReplyMarkup>()
+        {
+            {
+                nameof(KeyboardContents.BACK_KEY)
+                , new ReplyKeyboardMarkup(new KeyboardButton(KeyboardContents.BACK_KEY))
+            },
+            {
+                nameof(KeyboardContents.USER_IS_NOT_IN_CHANNEL)
+                , new ReplyKeyboardMarkup(new KeyboardButton(KeyboardContents.USER_IS_NOT_IN_CHANNEL))
+            },
+        };
+    }
+}

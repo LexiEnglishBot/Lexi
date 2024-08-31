250f9566ac8035a175f9ea5d7cce6b841b10cdf9
Matin_Ghanbari <ghanbarimatin6@gmail.com>
2024-08-31 04:19:14 +0330

    [LexiService]: Add validation service

---
+++ src/LexiService/Bootstrapper/DomainServices/ValidationServices.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationService.Services.BotManager;
+using Domain.Bot.Aggregate;
+using Domain.Bot.Validator;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bootstrapper.DomainServices;
+
+public static class ValidationServices
+{
+    public static IServiceCollection RegisterValidators(this IServiceCollection services)
+    {
+        return services.AddSingleton<IValidator<BotAggregate>, BotValidator>();
+    }
+
+    public static IServiceCollection RegisterBotManager(this IServiceCollection services)
+    {
+        return services.AddSingleton<IBotAggregateManager, BotAggregateManager>();
+    }
+}
